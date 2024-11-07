a327e6364c9b832a76388c6e8369ce8c5629829d
Allen Vu <allenvu4838@gmail.com>
2024-11-07 03:56:38 -0800

    barebones task component
    starter for task component for only client side but not tested and not bug-free

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,23 +1,29 @@
 import Link from "next/link";
+import { TaskStatus } from "./task";
 
 export default function Home() {
-    return (
-        <>
-            <div>
-                <b>Dashboard</b>
-            </div>
-            <div>
-                <Link href="/user">Go to user page</Link>  
-            </div>
-            <div>
-                <Link href="/rooms">Go to rooms page</Link>
-            </div>
-            <div>
-                <Link href="/shop">Go to shop page</Link>
-            </div>
-            <div>
-                <Link href="/">Sign out</Link>
-            </div>
-        </>
-    );
+  return (
+    <div className="flex">
+      <div className="w-64">
+        <div>
+          <b>Dashboard</b>
+        </div>
+        <div>
+          <Link href="/user">Go to user page</Link>
+        </div>
+        <div>
+          <Link href="/rooms">Go to rooms page</Link>
+        </div>
+        <div>
+          <Link href="/shop">Go to shop page</Link>
+        </div>
+        <div>
+          <Link href="/">Sign out</Link>
+        </div>
+      </div>
+      <div className="flex-1 ml-4">
+        <TaskStatus />
+      </div>
+    </div>
+  );
 }
