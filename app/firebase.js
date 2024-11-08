cacffd4502d1ba7ef9c67392c09b5519e74350d0
vatsnarsaria7 <vnarsaria@ucsd.edu>
2024-11-08 11:23:31 -0800

    changed firebase details

--- app/firebase.js
+++ app/firebase.js
@@ -1,18 +1,19 @@
+// src/firebaseConfig.js
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDR3FOpXrZH0E5pB8-rf6K2ys6Mt3N7PU4",
-  authDomain: "cse-110-group1.firebaseapp.com",
-  projectId: "cse-110-group1",
-  storageBucket: "cse-110-group1.firebasestorage.app",
-  messagingSenderId: "799846323818",
-  appId: "1:799846323818:web:d91da2f06944fc2d9d9c13",
-  measurementId: "G-CMSBLQ7YBX",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+export { app, analytics };
