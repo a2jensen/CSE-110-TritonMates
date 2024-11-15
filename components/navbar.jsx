2b43675bf1ae739662fe8cd2a67bbedffe52f4fd
y3do <y3do@ucsd.edu>
2024-11-14 20:28:37 -0800

    Navbar Style

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,15 +1,52 @@
+"use client";
+
+import Link from 'next/link';
 
 export default function Navbar() {
     return (
-        <>
-            <div><b>Navbar component</b></div>
-            <div>
-                <nav className="navbar">
+        <header className="navbar">
+            <div className="container">
+                <div className="logo">
+                    <Link href="/">
+                        <b>Navbar Component</b>
+                    </Link>
+                </div>
+                <nav className="nav-links">
                     <ul>
-                        <li><a href="">Test Element In Navbar</a></li>
+                        <li><Link href="/events">Events</Link></li>
+                        <li><Link href="/user">User Page</Link></li>
+                        <li><Link href="/rooms">Rooms</Link></li>
+                        <li><Link href="/shop">Shop</Link></li>
+                        <li><Link href="/signout">Sign Out</Link></li>
                     </ul>
                 </nav>
             </div>
-        </>
-    )
+            <style jsx>{`
+                .navbar {
+                    background: #333;
+                    color: #fff;
+                    padding: 1rem;
+                }
+                .container {
+                    display: flex;
+                    justify-content: space-between;
+                    align-items: center;
+                }
+                .logo {
+                    color: #fff;
+                    text-decoration: none;
+                    font-size: 1.2rem;
+                }
+                .nav-links ul {
+                    display: flex;
+                    list-style: none;
+                    gap: 1rem;
+                }
+                .nav-links ul li a {
+                    color: #fff;
+                    text-decoration: none;
+                }
+            `}</style>
+        </header>
+    );
 }
