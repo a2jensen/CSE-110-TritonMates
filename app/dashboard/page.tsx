0e9333d1fe01654b53cd78d3b287436a23dfd32d
y3do <y3do@ucsd.edu>
2024-11-17 21:08:32 -0800

    Navbar tweaks to help debug and rooms pages

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -10,23 +10,8 @@ export default function Home() {
         <div>
           <b>Dashboard</b>
         </div>
-            <div>
-                <Link href="/events">Go to events page</Link>  
-            </div>
-        <div>
-          <Link href="/user">Go to user page</Link>
-        </div>
-        <div>
-          <Link href="/rooms">Go to rooms page</Link>
-        </div>
-        <div>
-          <Link href="/shop">Go to shop page</Link>
-        </div>
-        <div>
-          <Link href="/">Sign out</Link>
-        </div>
       </div>
-      <UpcomingEvents roomName="testRoom" events={[]}/>
+      <UpcomingEvents roomName="testRoom" events={[]} />
       <div className="flex-1 ml-4">
         <TaskStatus />
       </div>
