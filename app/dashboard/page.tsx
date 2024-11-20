45be0b1142e2324357198d3c3476f7b4de7ce992
Allen Vu <allenvu4838@gmail.com>
2024-11-20 14:06:02 -0800

    tasks refactored into components structure and developing styling

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,34 +1,19 @@
 import UpcomingEvents from "../../components/events/upcomingEvents";
 import Link from "next/link";
-import { TaskStatus } from "./task";
-import Navbar from '../../components/navbar';
+import Navbar from "../../components/navbar";
+import TaskBoard from "../../components/tasks/taskBoard/taskBoard";
 
 export default function Home() {
   return (
-    <div className="flex">
-      <div className="w-64">
-        <div>
-          <b>Dashboard</b>
-        </div>
-            <div>
-                <Link href="/events">Go to events page</Link>  
-            </div>
-        <div>
-          <Link href="/user">Go to user page</Link>
-        </div>
-        <div>
-          <Link href="/rooms">Go to rooms page</Link>
-        </div>
-        <div>
-          <Link href="/shop">Go to shop page</Link>
-        </div>
-        <div>
-          <Link href="/">Sign out</Link>
-        </div>
+    <div className="min-h-screen bg-gray-50">
+      <div className="bg-white p-4 mb-4 flex items-center gap-4">
+        <div className="w-16 h-16 bg-gray-400 rounded-full"></div>
+        <h2 className="text-xl">User Profile</h2>
       </div>
-      <UpcomingEvents roomName="testRoom" events={[]}/>
-      <div className="flex-1 ml-4">
-        <TaskStatus />
+
+      <div className="space-y-6">
+        <TaskBoard />
+        <UpcomingEvents roomName="testRoom" events={[]} />
       </div>
     </div>
   );
