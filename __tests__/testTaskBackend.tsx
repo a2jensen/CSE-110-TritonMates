e6a8d8cf7f8bc2b964861973ff8c87ce682a3156
y3do <y3do@ucsd.edu>
2024-11-27 09:28:52 -0800

    Dummy test

--- __tests__/testTaskBackend.tsx
+++ __tests__/testTaskBackend.tsx
@@ -1,25 +1,17 @@
 import '@testing-library/jest-dom';
-import { render, screen, fireEvent, within,} from "@testing-library/react";
-import test,{ describe } from "node:test";
-import Home  from "../app/dashboard/page";
+import { render, screen, fireEvent, within, } from "@testing-library/react";
+import test, { describe } from "node:test";
+import Home from "../app/dashboard/page";
 import '@testing-library/jest-dom';
 
 // Dummy test 4 now
 
 describe("testing task functions", () => {
 
-    it('sanity check for page', () => {
-        render(<Home />);
-        const remaining = screen.getByText("Upcoming Events");
-    
-      
-        expect(remaining).toBeInTheDocument();
-    
-      
-      })
-
-  
-
+  it('sanity check for page', () => {
+    render(<Home />);
+    const remaining = screen.getByText("Upcoming Events");
+    expect(remaining).toBeInTheDocument();
+  })
 });
 
- 
--- __tests__/testUserProfile.tsx
+++ __tests__/testUserProfile.tsx
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom';
-import { render, screen, fireEvent, within,} from "@testing-library/react";
-import test,{ describe } from "node:test";
+import { render, screen, fireEvent, within, } from "@testing-library/react";
+import test, { describe } from "node:test";
 import UserPage from '@/app/user/page';
 import '@testing-library/jest-dom';
 
@@ -8,13 +8,9 @@ import '@testing-library/jest-dom';
 
 describe("testing user profile", () => {
 
-    it('Select Avatar text exists in page', () => {
-        render(<UserPage />);
-        expect(screen.getByText("Select Avdatar")).toBeInTheDocument();
-      })
-
-  
-
+  it('Select Avatar text exists in page', () => {
+    render(<UserPage />);
+    expect(screen.getByText("Select Avdatar")).toBeInTheDocument();
+  })
 });
 
- 
