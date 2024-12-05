bd3125ef8d7ffb2e0b3c9239fb865d3e06d83853
y <y3do@ucsd.edu>
2024-12-04 17:36:49 -0800

    Update testUserProfilePicture.tsx

--- __tests__/testUserProfilePicture.tsx
+++ __tests__/testUserProfilePicture.tsx
@@ -1,21 +1,36 @@
 import '@testing-library/jest-dom';
-import { render, screen, fireEvent, within,} from "@testing-library/react";
-import test,{ describe } from "node:test";
-import Home  from "../app/user/page";
+import { render, screen, fireEvent, within, } from "@testing-library/react";
+import test, { describe } from "node:test";
+import Home from "../app/user/page";
 import '@testing-library/jest-dom';
 
 
-describe("testing user pfp", () => {
+describe("testing user pfp page", () => {
 
-    it('points', () => {
-        render(<Home />);
-        expect(screen.getByText("Points")).toBeInTheDocument();
-    
-      
-      })
+  it('points exists sanity check', () => {
+    render(<Home />);
+    expect(screen.getByText("Points")).toBeInTheDocument();
+  })
 
-  
 
+  it('points exists sanity check', () => {
+    render(<Home />);
+    expect(screen.getByText("Points")).toBeInTheDocument();
+  })
+
+
+  describe('UserPage Component', () => {
+    it('increments points to 50 when "Earn Points" button is clicked', async () => {
+      render(<Home />);
+
+      const pointsDisplay = screen.getByText(/Points: 0/);
+      expect(pointsDisplay).toBeInTheDocument();
+
+      const earnPointsButton = screen.getByText('Earn Points');
+      fireEvent.click(earnPointsButton);
+
+      const updatedPointsDisplay = screen.getByText(/Points: 50/);
+      expect(updatedPointsDisplay).toBeInTheDocument();
+    });
+  });
 });
-
- 
