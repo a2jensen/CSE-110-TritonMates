98299f9aa3f43961bcd0aef7658ca5ddfc182bd0
a2jensen <a2jensen@ucsd.edu>
2024-12-05 14:08:53 -0800

    RoomSummary Context to API switch

--- components/rooms/roomSummary.tsx
+++ components/rooms/roomSummary.tsx
@@ -1,15 +1,42 @@
-
 'use client'
 import { useRoomContext } from "@/app/context/RoomContext";
-import { leaveRoom,  } from "@/app/api/rooms";
+import { leaveRoom, } from "@/app/api/rooms";
 import { checkUserAuth } from "@/app/api/user";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { get } from "http";
+import { checkRoom, fetchRoomData } from "@/app/api/rooms";
+import { useState } from "react";
 
-const RoomSummary : React.FC = () => {
+const RoomSummary: React.FC = () => {
+
+
+
+    const [roomData, setRoomData] = useState<any>(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const handleUserAuth = async () => {
+            const userData = await checkUserAuth();
+            const user_id = userData?.uid  || "";
+            const room_id = await checkRoom(user_id) || "";
+            const roomData = await fetchRoomData(room_id);
+            if (roomData) {
+                setRoomData(roomData);
+                setLoading(false);
+                console.log('Room data fetched successfully', roomData);
+            }
+        }
+        handleUserAuth();
+
+    }, []);
+
+
+
     const router = useRouter();
-    const { roomData } = useRoomContext();
+    // const { roomData } = useRoomContext();
 
-    console.log('Context information in roomSummary component',roomData);
+    console.log('Context information in roomSummary component', roomData);
     const leaveRoomAction = async () => {
         try {
             const roomCode = roomData?.room_code;
@@ -19,10 +46,18 @@ const RoomSummary : React.FC = () => {
                 await leaveRoom(roomCode, userId);
                 router.push("/rooms")
             }
-        } catch (error : unknown ) {
+        } catch (error: unknown) {
             console.error("Error trying to leave room: ", error)
         }
     }
+    if (loading) {
+        return (
+            <div>
+                Loading...
+            </div>
+        )
+    }
+
     return (
         <div className="p-4 m-10 flex justify-between">
             <div className="p-2">
